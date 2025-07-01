621159dc7581a67c2535a9d4a70af14e3411aec4
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-07-01 23:41:01 +0200

    wip: rust builder

--- examples/builder/rust/src/main.rs
+++ examples/builder/rust/src/main.rs
@@ -1,19 +1,39 @@
 
 struct Account {
-    id: &'static str,
+    id: String,
     name: Option<String>,
     suspended: bool
     
 }
 
 impl Account {
-    fn new(id: &'static str,) -> Self {
-        Self { id, name: None, suspended: false}
+    fn builder(id: String,) -> AccountBuilder {
+        AccountBuilder { id, name: None, suspended: false} // move default values into build?
     }
 }
 
 
+struct AccountBuilder {
+        id: String,
+    name: Option<String>,
+    suspended: bool
+
+}
+
+impl AccountBuilder {
+
+    fn build(self) -> Account {
+        Account { id: self.id, name: self.name, suspended: self.suspended}
+    }
+
+    fn name(mut self, name: String) -> AccountBuilder {
+        self.name = Some(name);
+        self
+    }
+}
+
 
 fn main() {
-    let a = Account::new("test");
+    let mut a = Account::builder("test".to_owned()).build();
+    a.name = None // TODO: forbid
 }
