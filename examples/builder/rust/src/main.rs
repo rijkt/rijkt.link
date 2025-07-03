5523e0e78c64285c786f04494a4dda84e1648c5e
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-07-03 21:32:06 +0200

    change rust model

--- examples/builder/rust/src/main.rs
+++ examples/builder/rust/src/main.rs
@@ -1,39 +1,58 @@
-
-struct Account {
-    id: String,
+struct Tofu {
+    id: u16,
     name: Option<String>,
-    suspended: bool
-    
+    silky: bool,
+    smoky: bool,
 }
 
-impl Account {
-    fn builder(id: String,) -> AccountBuilder {
-        AccountBuilder { id, name: None, suspended: false} // move default values into build?
+impl Tofu {
+    fn builder(id: u16) -> TofuBuilder {
+        TofuBuilder::new(id)
     }
 }
 
-
-struct AccountBuilder {
-        id: String,
+struct TofuBuilder {
+    id: u16,
     name: Option<String>,
-    suspended: bool
-
+    silky: bool,
+    smoky: bool,
 }
 
-impl AccountBuilder {
-
-    fn build(self) -> Account {
-        Account { id: self.id, name: self.name, suspended: self.suspended}
+impl TofuBuilder {
+    fn new(id: u16) -> TofuBuilder {
+        TofuBuilder {
+            id,
+            name: None,
+            silky: false,
+            smoky: false,
+        }
     }
 
-    fn name(mut self, name: String) -> AccountBuilder {
+    fn build(self) -> Tofu {
+        Tofu {
+            id: self.id,
+            name: self.name,
+            silky: self.silky,
+            smoky: self.smoky,
+        }
+    }
+
+    fn name(mut self, name: String) -> TofuBuilder {
         self.name = Some(name);
+        self
+    }
+
+    fn smoky(mut self, smoky: bool) -> TofuBuilder {
+        self.silky = smoky;
         self
     }
 }
 
-
 fn main() {
-    let mut a = Account::builder("test".to_owned()).build();
-    a.name = None // TODO: forbid
+    let smoky_tofu = Tofu::builder(0)
+        .name("Smokey smoke tofu".to_owned())
+        .smoky(true)
+        .build();
+
+    let silken_tofy = Tofu::builder(1).smoky(true).build();
 }
